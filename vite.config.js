a13141694a59e8decb4fb81cb1245b7248d1495e
oppai <hotsoup.h@gmail.com>
2025-05-04 20:56:27 +0900

    syrink js files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   build: {
-    outDir: 'docs'
-  }
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/three')) {
+            return 'vendor-three';
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    visualizer({
+      open: true,
+      filename: 'stats.html',
+    }),
+  ]
 }); 
